2f40c1152e11466911e2f52e951eac790d009956
Your NameBuysschaertQuentin <quentin.buysschaert@gmail.com>
2023-06-05 17:09:48 +0200

    feat: add users_entity

--- src/users/dto/create-user.input.ts
+++ src/users/dto/create-user.input.ts
@@ -1,1 +1,23 @@
-export class CreateUserInput {}
+import { Field, InputType } from '@nestjs/graphql';
+import { User } from '../entities/user.entity';
+
+@InputType()
+export class CreateUserInput extends User {
+  @Field()
+  email: string;
+
+  @Field()
+  firstname: string;
+
+  @Field()
+  lastname: string;
+
+  @Field()
+  password: string;
+
+  @Field()
+  isBanned: boolean;
+
+  @Field()
+  role_id: string;
+}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,24 @@
-export class User {}
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  firstname: string;
+
+  @Column()
+  lastname: string;
+
+  @Column()
+  password: string;
+
+  @Column({ default: true })
+  isBanned: boolean;
+
+  @Column()
+  role_id: string;
+}
