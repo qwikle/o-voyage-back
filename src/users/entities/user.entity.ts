55ae92573ec4dc27ca6e484236eb2ff6b085d4c7
Qwikle <qwikle.ziko@gmail.com>
2023-06-05 18:19:22 +0200

    feat: add Hash password to user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,6 +1,18 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BeforeInsert,
+  BeforeUpdate,
+  AfterLoad,
+} from 'typeorm';
+import { HashContract, Hash } from 'src/commons/bcrypt';
 @Entity()
 export class User {
+  private readonly hash: HashContract;
+  constructor() {
+    this.hash = Hash.getInstance();
+  }
   @PrimaryGeneratedColumn()
   id: string;
 
@@ -21,4 +33,18 @@ export class User {
 
   @Column()
   role_id: string;
+
+  private tempPassword: string;
+  @AfterLoad()
+  private loadTempPassword(): void {
+    this.tempPassword = this.password;
+  }
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  async hashPassword(): Promise<void> {
+    if (this.tempPassword !== this.password) {
+      this.password = await this.hash.hashPassword(this.password);
+    }
+  }
 }
