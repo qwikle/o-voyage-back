e0a0adc1682494522dfc72cb8fd24702e9c0233f
Qwikle <qwikle.ziko@gmail.com>
2023-06-05 17:11:31 +0200

    feat: Add typeOrm and GraphQL configuration to app module

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: { origin: '*' } });
+  await app.listen(process.env.PORT || 3000, () => {
+    console.log(
+      `Server running at http://localhost:${process.env.PORT || 3000}\n`,
+      `GraphQL running at http://localhost:${process.env.PORT || 3000}/graphql`,
+    );
+  });
 }
 bootstrap();
