742c291e04e3f393a835feee5e68e81c87134261
Your NameBuysschaertQuentin <quentin.buysschaert@gmail.com>
2023-06-07 00:19:10 +0200

    feat: add email scalar

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,9 +4,10 @@ import { UsersResolver } from './users.resolver';
 import { LoaderService } from 'src/commons/dataloader';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { EmailScalar } from 'src/scalars/email';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [UsersResolver, UsersService, LoaderService],
+  providers: [UsersResolver, UsersService, LoaderService, EmailScalar],
 })
 export class UsersModule {}
