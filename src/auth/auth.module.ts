7367eef9526168a51114f446b38e72d78ff3aa6f
Qwikle <qwikle.ziko@gmail.com>
2023-06-07 18:01:21 +0200

    feat: add jwt module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,17 @@ import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      global: true,
+      secret: process.env.KEY,
+      signOptions: { expiresIn: '15m' },
+    }),
+  ],
   providers: [AuthResolver, AuthService],
 })
 export class AuthModule {}
