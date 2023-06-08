3c35d9da9864c5447599413347ad998890e4e4aa
Your NameBuysschaertQuentin <quentin.buysschaert@gmail.com>
2023-06-08 11:37:43 +0200

    feat: verify password no complete
    database error

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -2,14 +2,33 @@ import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { UsersService } from './users.service';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
+import { GraphQLError } from 'graphql';
 
 @Resolver('User')
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
   @Mutation('createUser')
-  async create(@Args('createUserInput') createUserInput: CreateUserInput) {
-    return this.usersService.createUser(createUserInput);
+  async create(@Args('createUserInput') createUserInput: CreateUserInput ) {
+    const user = await this.usersService.findByEmail(createUserInput.email);
+    if (user) {
+      throw new GraphQLError('This email is allready used', {
+        extensions: {
+          code: 'BAD_USER_INPUT',
+          argumentName: 'email'
+        }
+      }
+    )};
+    if (createUserInput.password !== createUserInput.confirmPassword) {
+      throw new GraphQLError('Password and confirm password are not the same', {
+        extensions: {
+          code: 'BAD_USER_INPUT',
+          argumentName: 'confirmPassword'
+        }
+      })
+    };
+    const createdUser = await this.usersService.createUser(createUserInput);
+    return {user : createdUser};
   }
 
   @Query('users')
