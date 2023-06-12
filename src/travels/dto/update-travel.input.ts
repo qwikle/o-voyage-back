c60e696e9727adaf772c00fceab01eaafad64b73
Your NameBuysschaertQuentin <quentin.buysschaert@gmail.com>
2023-06-12 11:58:12 +0200

    feat: modify updatetravelinput

--- src/travels/dto/update-travel.input.ts
+++ src/travels/dto/update-travel.input.ts
@@ -4,26 +4,6 @@ import { PartialType } from '@nestjs/mapped-types';
 
 @InputType()
 export class UpdateTravelInput extends PartialType(CreateTravelInput) {
+  @Field()
   id: number;
-
-  @Field()
-  title: string;
-
-  @Field()
-  from: string;
-
-  @Field()
-  to: string;
-
-  @Field()
-  departureDate: Date;
-
-  @Field()
-  arrivalDate: Date;
-
-  @Field()
-  budget: number;
-
-  @Field()
-  numberOfAttendees: number;
 }
