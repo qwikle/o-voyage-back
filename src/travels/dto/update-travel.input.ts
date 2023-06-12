7c9e72f83a4e54b371d539285f7941176a4c485e
Your NameBuysschaertQuentin <quentin.buysschaert@gmail.com>
2023-06-12 11:52:20 +0200

    feat: createtravelinput is ok and complete travel graphql

--- src/travels/dto/update-travel.input.ts
+++ src/travels/dto/update-travel.input.ts
@@ -1,6 +1,29 @@
+import { Field, InputType } from '@nestjs/graphql';
 import { CreateTravelInput } from './create-travel.input';
 import { PartialType } from '@nestjs/mapped-types';
 
+@InputType()
 export class UpdateTravelInput extends PartialType(CreateTravelInput) {
   id: number;
+
+  @Field()
+  title: string;
+
+  @Field()
+  from: string;
+
+  @Field()
+  to: string;
+
+  @Field()
+  departureDate: Date;
+
+  @Field()
+  arrivalDate: Date;
+
+  @Field()
+  budget: number;
+
+  @Field()
+  numberOfAttendees: number;
 }
