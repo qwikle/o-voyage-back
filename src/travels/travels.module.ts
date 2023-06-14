b49c56333849094817f1c4b7d981f1dc24ce822c
Qwikle <qwikle.ziko@gmail.com>
2023-06-14 14:32:01 +0200

    refactor: move scalar forlder into commons and context type in forlder type

--- src/travels/travels.module.ts
+++ src/travels/travels.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TravelsService } from './travels.service';
 import { TravelsResolver } from './travels.resolver';
-import { DateScalar } from 'src/scalars/date';
+import { DateScalar } from 'src/commons/scalars/date';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Travel } from './entities/travel.entity';
 import { User } from 'src/users/entities/user.entity';
