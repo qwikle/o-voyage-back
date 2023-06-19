5a2cd6122a249aed6654e3e2ac1346612990112a
Your NameBuysschaertQuentin <quentin.buysschaert@gmail.com>
2023-06-19 12:08:32 +0200

    feat: complete entity categorie and relation in activity

--- src/categories/entities/category.entity.ts
+++ src/categories/entities/category.entity.ts
@@ -1,1 +1,14 @@
-export class Category {}
+import { Activity } from 'src/graphql';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+
+export class Category {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @ManyToOne(() => Category, (category) => category.id)
+    activity: Activity;
+
+}
